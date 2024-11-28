b7f57095f3230cbd13fa630b0f0fb24c6cfdd049
LuisDgzA <luisdgza96@gmail.com>
2024-11-27 18:15:04 -0600

    add clock

--- main.js
+++ main.js
@@ -337,3 +337,33 @@ Items.forEach(item => {
         localStorage.setItem("owned", JSON.stringify(idsOwned));
     })
 })
+
+
+function showTime(){
+    let date = new Date();
+    let h = date.getHours(); // 0 - 23
+    
+   
+    let session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    
+    
+    let time = h + " " + session;
+    document.getElementById("clock").innerText = time;
+    // document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
